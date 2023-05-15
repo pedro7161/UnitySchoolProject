fadae583d06c6b45dc37d3302599d1e0568f60cf
Pedro Pinto <pedro.ramos9@hotmail.com>
2023-05-15 20:37:18 +0100

    fetch quest ongoing2

--- Assets/Scripts/questmanager.cs
+++ Assets/Scripts/questmanager.cs
@@ -8,7 +8,14 @@ public class questmanager : MonoBehaviour
 
     // Add variables and methods to manage quests
     // For example, you could have a list of active quests and methods to start/complete quests.
-    
+   
+
+    // Add variables and methods to manage quests
+    // For example, you could have a list of active quests and methods to start/complete quests.
+
+    // Event for quest start
+    public delegate void QuestStartedDelegate(int questId);
+    public event QuestStartedDelegate OnQuestStarted;
     private void Awake()
     {
         if (instance == null)
