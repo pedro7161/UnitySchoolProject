15783e040d9de5d8f3c27088afa51969cc2ef5be
Pedro Pinto <pedro.ramos9@hotmail.com>
2023-06-11 22:07:42 +0100

    feat:menu code

---
+++ Assets/loadmap.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadmap : MonoBehaviour
+{
+    public string mapname;
+    public void loadmap1()
+    {
+        SceneManager.LoadScene(mapname);
+    }
+}
