ea0abfbc0841c0db54a17f97bed773259520cc7a
Pedro Pinto <pedro.ramos9@hotmail.com>
2023-06-12 14:16:07 +0100

    feat:script for new menu

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
-   public Canvas menu;
-   private void Start() {
+    public Canvas menu;
+
+    private void Start()
+    {
         menu.enabled = false;
-   }
-   private void FixedUpdate() {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-             menu.enabled = !menu.enabled;
+        SetMouseVisibility(true);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menu.enabled = !menu.enabled;
+            SetMouseVisibility(!menu.enabled);
         }
-   }
+    }
+
+    private void SetMouseVisibility(bool isVisible)
+    {
+        Cursor.visible = isVisible;
+        Cursor.lockState = isVisible ? CursorLockMode.None : CursorLockMode.Locked;
+    }
 }
